bcd983b23380a7d71e7238cf2f145b21fb1cb7ca
mateus tenorio <mateus.tenorio@hotmail.com>
2023-11-01 10:59:15 -0300

    destroy

--- terraform/azure/provider.tf
+++ terraform/azure/provider.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "rg-staticsite-tf"
+    resource_group_name  = "ResourceGroup"
     storage_account_name = "StorageAccount"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
